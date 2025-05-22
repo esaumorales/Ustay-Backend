9b3c38bbf7eb5098b498cc992aac34bc17e4e181
esaumorales <esaumorales2322@gmail.com>
2025-05-22 01:00:31 -0500

    deployd railway

--- src/config.js
+++ src/config.js
@@ -19,6 +19,6 @@ module.exports = {
   googleAuth: {
     clientID: process.env.GOOGLE_CLIENT_ID || '',
     clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3000/auth/google/callback',
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || '',
   }
 };
